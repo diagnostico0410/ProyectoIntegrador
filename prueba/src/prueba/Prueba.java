2c7f4e8232a394505dd2c0d1d4552a6f166718b9
diagnostico0410 <57378967+diagnostico0410@users.noreply.github.com>
2019-12-15 22:19:05 -0600

    Update Prueba.java

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -15,7 +15,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("delta");
+        System.out.println("delta uno");
         System.out.println("fallout");
         System.out.println("faltante");
     }
